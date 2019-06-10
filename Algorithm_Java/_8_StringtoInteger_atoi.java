6bf05af146fc84ae0bc76be5c44cf2bc190d6702
Mecewe <mecewe@outlook.com>
2019-06-10 16:03:20 +0800

    atoi

---
+++ Algorithm_Java/_8_StringtoInteger_atoi.java
@@ -0,0 +1,89 @@
+package com.mecewe.Leetcode;
+
+/**
+ *8. String to Integer (atoi)
+
+         Implement atoi which converts a string to an integer.
+         The function first discards as many whitespace characters as necessary until the first non-whitespace character is found.
+         Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible,
+         and interprets them as a numerical value.
+
+         The string can contain additional characters after those that form the integral number,
+         which are ignored and have no effect on the behavior of this function.
+
+         If the first sequence of non-whitespace characters in str is not a valid integral number,
+         or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+         If no valid conversion could be performed, a zero value is returned.
+
+         Note:
+
+         Only the space character ' ' is considered as whitespace character.
+         Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range:
+         [−2^31,  2^31 −1]. If the numerical value is out of the range of representable values,
+         INT_MAX (2^31 −1) or INT_MIN (−2^31) is returned.
+
+         Example 1:
+         Input: "42"
+         Output: 42
+
+         Example 2:
+         Input: "   -42"
+         Output: -42
+         Explanation: The first non-whitespace character is '-', which is the minus sign.
+         Then take as many numerical digits as possible, which gets 42.
+
+         Example 3:
+         Input: "4193 with words"
+         Output: 4193
+         Explanation: Conversion stops at digit '3' as the next character is not a numerical digit.
+
+         Example 4:
+         Input: "words and 987"
+         Output: 0
+         Explanation: The first non-whitespace character is 'w', which is not a numerical
+         digit or a +/- sign. Therefore no valid conversion could be performed.
+
+         Example 5:
+
+         Input: "-91283472332"
+         Output: -2147483648
+         Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
+         Thefore INT_MIN (−231) is returned.
+ */
+
+public class _8_StringtoInteger_atoi {
+    public int myAtoi(String str) {
+        int tag = 0;
+        Boolean isPos = true;
+        long sum = 0;
+        for(int i=0;i<str.length();i++){
+
+            if(tag==0 &&str.charAt(i) == ' '){
+                continue;
+            }else if (tag==0 && (str.charAt(i) == '+'|| str.charAt(i) == '-')){
+//                if(str.charAt(i+1) >= '0' && str.charAt(i+1) <= '9'){
+                    isPos = (str.charAt(i) == '+');
+                    tag=1;
+//                }
+            }else {
+                boolean b = str.charAt(i) >= '0' && str.charAt(i) <= '9';
+                if(b){
+                    tag = 1;
+                    if((sum*10+str.charAt(i)-'0')>(long)Integer.MAX_VALUE+1){
+                        return isPos?Integer.MAX_VALUE:Integer.MIN_VALUE;
+                    }
+                    sum = sum*10+str.charAt(i)-'0';
+                }else{
+                    break;
+                }
+            }
+        }
+        if(!isPos){
+            sum = (-1*sum < Integer.MIN_VALUE)? Integer.MIN_VALUE:(-1)*sum;
+        }else {
+            sum = (sum>Integer.MAX_VALUE)?Integer.MAX_VALUE:sum;
+        }
+        return (int)sum;
+    }
+}
